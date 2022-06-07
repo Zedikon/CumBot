a59ff85c89b9de14797b76694fd2c2dfe72513f3
Seeroy <37861929+Seeroy@users.noreply.github.com>
2022-06-07 18:58:04 +0300

    upd

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 const mineflayer = require('mineflayer')
+const phrases = ['Привет, я бот', 'Я бот, разработанный Seeroy и Zedikon', 'ты ботяра а я бот'];
+
+console.log("Starting bot");
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 const bot = mineflayer.createBot({
   host: 'justanarchy.tk',
-  username: 'loshok',
+  username: 'CumBot',
   version: "1.12.2"
 })
 
-bot.on('chat', (username, message) => {
-  if (username === bot.username) return
-  bot.chat(message)
+bot.on('chat', function (username, message) {
+  console.log(message);
+})
+
+bot.once('spawn', () => {
+  bot.chat("/login 123123");
+  console.log("I`m spawned and ready!");
+  send = phrases[getRandomInt(0,2)];
+  bot.chat("/say " + send);
+  console.log("Im sending:", send);
 })
 
 // Log errors and kick reasons:
